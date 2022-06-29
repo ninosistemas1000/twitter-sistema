d21704dbaea669cadb50763c66afac37f89f77d7
ninosistemas1000 <ninosistemas@icloud.com>
2022-06-30 01:23:54 +0200

    primer commit

---
+++ bd/conexionBD.go
@@ -0,0 +1,35 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://nino:iXArmKwsG4N47P2Q@cluster0.8jwdk7a.mongodb.net/test")
+
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexion Exitosa con la BD")
+	return client
+}
+
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
